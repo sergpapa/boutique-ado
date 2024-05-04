d3643fe5aafff8f27eed8bed4af683307d83b15f
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-05-04 10:30:11 +0000

    home template, base template, base css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+wo+x)x(v&85(l_czk#!n4=e*=un#g=4*^#jwu$14!uwjy%n6u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-sergpapa-boutiqueado-in751m06r1d.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-sergpapa-boutiqueado-4a8arv8umgn.ws-eu111.gitpod.io']
 
 
 # Application definition
