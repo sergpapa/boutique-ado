52e084c5feb60599f245c6b5bd5d62d2a08bdc61
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-05-05 12:29:00 +0000

    finalised products page

--- products/views.py
+++ products/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
 
 # Create your views here.
+
 def all_products(request):
-    """ A view to show all products, including sorting and making queries """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
@@ -14,24 +16,23 @@ def all_products(request):
 
     if request.GET:
         if 'sort' in request.GET:
-                sortkey = request.GET['sort']
-                sort = sortkey
-                if sortkey == "name":
-                    sortkey == "lower_name"
-                    products = products.annotate(lower_name=Lower('name'))
-
-
-                if 'direction' in request.GET:
-                    direction = request.GET['direction']
-                    if direction == 'desc':
-                        sortkey = f'{sortkey}'
-                products = products.order_by(sortkey)
-
-
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
         if 'category' in request.GET:
-            categories = request.GET['category'].split(",")
+            categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
-            catgories = Category.objects.filter(name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -48,10 +49,11 @@ def all_products(request):
         'products': products,
         'search_term': query,
         'current_categories': categories,
-        'current_sorting' : current_sorting,
+        'current_sorting': current_sorting,
     }
 
-    return render(request, "products/products.html", context)
+    return render(request, 'products/products.html', context)
+
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
@@ -59,7 +61,7 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        "product" : product,
+        'product': product,
     }
 
-    return render(request, "products/product_detail.html", context)
+    return render(request, 'products/product_detail.html', context)
