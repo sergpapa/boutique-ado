652371ec00320b6210ec9925e1fa9496cdcfab34
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-05-18 09:50:50 +0000

    checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
+from .forms import OrderForm
 
 # Create your views here.
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, 'There is nothing currently in your bag')
+        return redirect(reverse('products'))
+    
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form
+    }
+
+    return render(request, template, context)
